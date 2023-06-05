15995b072997fa8e08b56736e64db80e87d89cfb
Todev3 <emerson_abedias@hotmail.com>
2023-06-04 23:48:16 -0300

    adiciona db config, dockerfile e docker compose file pra ambiente de dev

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'pifio',
+      entities: [__dirname + '/**/*.entity{.ts}'],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
